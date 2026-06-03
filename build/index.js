4653cbab16b7f024825009fea28b267abdef453e
Azzam S.A <noreply@azzamsa.com>
2026-06-03 09:22:31 +0700

    fix: inactive selection is not distinguishable

--- build/index.js
+++ build/index.js
@@ -6,8 +6,8 @@ const plugin = { themes: [{
 	dark: true,
 	base: {
 		surface: "#000000",
-		surfaceHighlight: "#5a5a5a",
-		surfaceActive: "#2f3849",
+		surfaceHighlight: "#1d2235",
+		surfaceActive: "#2b3045",
 		border: "#646464",
 		borderSubtle: "#303030",
 		text: "#ffffff",
@@ -21,30 +21,6 @@ const plugin = { themes: [{
 		warning: "#fec43f",
 		danger: "#ff5f59",
 		selection: "#5a5a5a"
-	},
-	components: {
-		dialog: { surface: "#1e1e1e" },
-		sidebar: {
-			surface: "#000000",
-			border: "#646464"
-		},
-		appHeader: {
-			surface: "#000000",
-			border: "#646464"
-		},
-		responsePane: {
-			surface: "#000000",
-			border: "#646464"
-		},
-		button: {
-			primary: "#00d3d0",
-			secondary: "#b6a0ff",
-			info: "#00d3d0",
-			success: "#44bc44",
-			notice: "#d0bc00",
-			warning: "#fec43f",
-			danger: "#ff5f59"
-		}
 	}
 }] };
 
--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ export const plugin: PluginDefinition = {
       dark: true,
       base: {
         surface: "#000000",
-        surfaceHighlight: '#5a5a5a',
+        surfaceHighlight: '#1d2235',
         // active selection on sidebar
-        surfaceActive: '#2f3849',
+        surfaceActive: '#2b3045',
         border: '#646464',
         borderSubtle: '#303030',
         text: "#ffffff",
@@ -23,33 +23,6 @@ export const plugin: PluginDefinition = {
         warning: "#fec43f",
         danger: "#ff5f59",
         selection: "#5a5a5a",
-      },
-      components: {
-        dialog: {
-          surface: "#1e1e1e",
-        },
-        sidebar: {
-          surface: "#000000",
-          border: "#646464",
-        },
-        appHeader: {
-          surface: "#000000",
-          border: "#646464",
-        },
-        responsePane: {
-          surface: "#000000",
-          border: "#646464",
-        },
-        button: {
-          primary: "#00d3d0",
-          // icons on the menus
-          secondary: "#b6a0ff",
-          info: "#00d3d0",
-          success: "#44bc44",
-          notice: "#d0bc00",
-          warning: "#fec43f",
-          danger: "#ff5f59",
-        },
       },
     },
   ],
